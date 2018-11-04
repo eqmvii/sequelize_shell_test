e6a2a43b3b7ee1d43b2cec4192fe341e0c74d808
Eric (Martin) Mancini <coelomate@gmail.com>
2018-11-04 18:25:54 -0500

    hbs view woo spring break

--- server.js
+++ server.js
@@ -21,21 +21,13 @@ var db = require("./models");
 db.sequelize.sync().then(function(){
   console.log("DB connected!");
 
-  // Create dummy data every time the server is started, for... reasons
-  db.User.create({
-    email: "fake@email.com",
-    password: "password"
-  })
-
-  // Log just the first user lol idk
-  db.User.findAll().then(function(result) {
-    console.log
-   console.log(result[0].dataValues);
-  });
-
   // Use Handlebars to render the main index.html page with the movies in it.
   app.get("/", function(req, res) {
-    res.render("index", { test: "walrus" });
+    db.User.findAll().then(function(result) {
+      console.log("Users length:" + result.length);
+      console.log(result[0].dataValues);
+      res.render("index", { users: result });
+    });
   });
 
   app.listen(PORT, function() {
