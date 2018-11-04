5bef041c916dfaf6a75769189ccc942e2bc4ca7a
Eric (Martin) Mancini <coelomate@gmail.com>
2018-11-04 18:21:03 -0500

    more handlebars stuff

--- server.js
+++ server.js
@@ -8,6 +8,14 @@ app.use(express.json());
 
 app.use(express.static("app/public"));
 
+// Handlebars Goodness
+
+var exphbs = require("express-handlebars");
+
+// app.set("views", __dirname + "\\app\\views"); // TODO: Figure this out or not idk
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
 // Sequelize Goodness
 var db = require("./models");
 db.sequelize.sync().then(function(){
@@ -23,6 +31,11 @@ db.sequelize.sync().then(function(){
   db.User.findAll().then(function(result) {
     console.log
    console.log(result[0].dataValues);
+  });
+
+  // Use Handlebars to render the main index.html page with the movies in it.
+  app.get("/", function(req, res) {
+    res.render("index", { test: "walrus" });
   });
 
   app.listen(PORT, function() {
